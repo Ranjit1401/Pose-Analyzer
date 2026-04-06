d53c8d0ba76db7ced9af520fa47f779160f10798
Ranjit1401 <ranjitbhardwaj77@gmail.com>
2026-04-06 20:03:38 +0530

    fixed backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://postureai-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
